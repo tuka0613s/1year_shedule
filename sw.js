fc85d1f34ee0f63ecdf3472a83a33a9b5dc9a94b
tuka8s <tuka8s@gmail.com>
2026-04-27 14:29:45 +0900

    Update sw.js to v5 with skipWaiting to fix disappearing refresh button

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'sche-v1';
+const CACHE_NAME = 'sche-v5';
 const ASSETS = [
   './',
   './index.html',
@@ -9,6 +9,7 @@ const ASSETS = [
 
 // Install Event
 self.addEventListener('install', (e) => {
+  self.skipWaiting(); // すぐに新しいサービスワーカーを有効にする
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(ASSETS);
@@ -19,15 +20,18 @@ self.addEventListener('install', (e) => {
 // Activate Event
 self.addEventListener('activate', (e) => {
   e.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(
-        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
-      );
-    })
+    Promise.all([
+      self.clients.claim(), // すぐに制御を開始する
+      caches.keys().then((keys) => {
+        return Promise.all(
+          keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+        );
+      })
+    ])
   );
 });
 
-// Fetch Event
+// Fetch Event (Cache First for stability)
 self.addEventListener('fetch', (e) => {
   e.respondWith(
     caches.match(e.request).then((res) => {
